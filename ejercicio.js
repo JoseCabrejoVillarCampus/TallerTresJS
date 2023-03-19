5a3a1b51b84d603b40290cb6da67c3543e003b54
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-03-19 13:39:02 -0500

    feat: :zap: Trata de solucionar las relaciones entre formularios

--- ejercicio.js
+++ ejercicio.js
@@ -1,28 +1,3 @@
-/* 1. Construir un objeto literal "campus" que gestione
-la info(PROPIEDADES) de Campus, trainers, campers, niveles,
-tecnologías, teams y roadMap
-1.1. De campus administrar los datos de contacto de las sedes en
-Bucaramanga, Bogotá, Medellín y México
-1.2. De los trainers y campers, su nombre, sus teléfonos, teams
-(horarios de las teams=> día, hora y salones (nro y piso), y el
-email, y de los campers también horarios de inglés y ser.
-1.3. De los campers, también gestionar su nivel actual, como su
-barrio y medio de transporte
-1.4. De los niveles, su pre requisito, a que tecnología pertenece, si
-es electiva u obligatoria
-1.5. De la roadmap , Nro de créditos, año, Nro de asignaturas */
-/* 2. Consultas: Usando Destructuring,
-2.1 De los trainers, reportar si la asignatura (tecnología) es
-remota o presencial y de los campers el nombre de salón.
-2.2 El teléfono de la sede de Medellín y la dirección de la
-sede de Bucaramanga
-2.3 De la asignatura (tecnología) si tiene sandbox o no */
-/* 3. Consultas: Usando sintaxis de punto.
-3.1 Reportar, prerequisito de la asignatura (tecnología) y nro de
-créditos del roadmap
-3.2 Agregar mas objetos con mas objetos anidados de manera libre
-(por lo menos 7) */
-
 let myFormularioCampus = document.querySelector("#myFormularioCampus");
 let myFormularioCampers = document.querySelector("#myFormularioCampers");
 let myFormularioTrainers = document.querySelector("#myFormularioTrainers");
@@ -67,7 +42,6 @@ myFormularioNiveles.addEventListener("submit", (e)=>{
     let sedeNivel = data.sedeNivel;
     delete data.sedeNivel;
     campus[`${sedeNivel}`]["Niveles"].unshift(data); 
-    console.log(data.preRequisito);
     listar1('[name="nivelCamper"]', (Object.values(data)));
     listar1('[name="nivelTrainer"]',(Object.values(data)));
     myFormularioNiveles.reset();
@@ -80,7 +54,8 @@ myFormularioRoadmap.addEventListener("submit", (e)=>{
     let sedeRoadmap = data.sedeRoadmap;
     delete data.sedeRoadmap;
     campus[`${sedeRoadmap}`]["Roadmap"].unshift(data);
-    // listar1('[name="asignaturaTrainer"]', (Object.values(data)));
+    console.log(data);
+    listar1('[name="asignaturaTrainer"]', (Object.values(data)));
     myFormularioRoadmap.reset();
 });
 
@@ -92,7 +67,6 @@ myFormularioCampers.addEventListener("submit", (e)=>{
     campus[`${sedeCampers}`]["Camper"].unshift(data);
     myFormularioCampers.reset();
 })
-
 
 myFormularioTrainers.addEventListener("submit", (e)=>{
     e.preventDefault();
